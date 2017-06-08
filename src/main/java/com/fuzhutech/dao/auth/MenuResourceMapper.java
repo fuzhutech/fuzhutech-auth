283976c9b9cd434dff453b276042e4d2ef10a8f2
fuzhutech <fuzhutech@163.com>
2017-06-08 18:13:00 +0800

    删除余留的表

--- src/main/java/com/fuzhutech/dao/auth/MenuResourceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fuzhutech.dao.auth;
-
-import com.fuzhutech.common.dao.BaseMapper;
-import com.fuzhutech.entity.auth.MenuResource;
-
-public interface MenuResourceMapper extends BaseMapper<MenuResource> {
-}
--- src/main/java/com/fuzhutech/dao/auth/PermissionGroupMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fuzhutech.dao.auth;
-
-import com.fuzhutech.common.dao.BaseMapper;
-import com.fuzhutech.entity.auth.PermissionGroup;
-
-public interface PermissionGroupMapper extends BaseMapper<PermissionGroup> {
-}
